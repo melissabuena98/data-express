a27af61ba8faaefdde904185cc8d79dd416cca8a
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2017-11-23 21:02:31 -0700

    Nothing much

---
+++ public/styles/script.js
@@ -0,0 +1,33 @@
+var canvas = document.getElementById('demo');
+var ctx = canvas.getContext('2d');
+/*
+var grid = [];
+var MAZE_WIDTH = 25;
+var MAZE_HEIGHT = 25;
+var BLOCK_SIZE = 20;
+
+function drawSquare(x, y, r, g, b){
+    ctx.fillStyle = "rgb("+ r +", "+ g +", "+ b +")";
+    ctx.fillRect(x, y, BLOCK_SIZE-1, BLOCK_SIZE-1);
+}
+//Create maze
+for (i = 0; i < MAZE_WIDTH; i++) {
+    grid[i] = [];
+    for (j = 0; j < MAZE_HEIGHT; j++) {
+        grid[i][j] = 1;
+    }
+}
+function drawMaze(){
+    for (var y = 0; y < MAZE_HEIGHT; y++) {
+        for (x = 0; x < MAZE_WIDTH; x++) {
+            if(x%2 == 1 && y%2 == 1){
+                    grid[x][y] = 0;
+            }
+            if(grid[x][y] == 1){
+                drawSquare(x*BLOCK_SIZE, y*BLOCK_SIZE, 0, 255, 255);
+            }
+        }
+    }
+}
+drawMaze();
+*/
