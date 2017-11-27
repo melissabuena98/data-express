b0006a8b979bfc21e3c161e3580d60f7b14f2461
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2017-11-26 22:59:41 -0700

    Temp bar chart

--- public/styles/script.js
+++ public/styles/script.js
@@ -1,33 +1,18 @@
-var canvas = document.getElementById('demo');
-var ctx = canvas.getContext('2d');
-/*
-var grid = [];
-var MAZE_WIDTH = 25;
-var MAZE_HEIGHT = 25;
-var BLOCK_SIZE = 20;
+window.onload = function(){
+    var canvas = document.getElementById('demo');
+    var ctx = canvas.getContext('2d');
+    
+    var scores = [100, 90, 80, 60, 40, 20];
+    var width = 50;
+    var curX = 50;
+    var base = 200;
+    
+    for(i = 0; i < scores.length; i++){
+        var height = scores[i];
+        ctx.fillStyle = "rgb(20,"+(i*120)+",240)";
+        ctx.fillRect(curX, canvas.height - height, width, height);
+        curX += width + 10;
+        
+    }
 
-function drawSquare(x, y, r, g, b){
-    ctx.fillStyle = "rgb("+ r +", "+ g +", "+ b +")";
-    ctx.fillRect(x, y, BLOCK_SIZE-1, BLOCK_SIZE-1);
 }
-//Create maze
-for (i = 0; i < MAZE_WIDTH; i++) {
-    grid[i] = [];
-    for (j = 0; j < MAZE_HEIGHT; j++) {
-        grid[i][j] = 1;
-    }
-}
-function drawMaze(){
-    for (var y = 0; y < MAZE_HEIGHT; y++) {
-        for (x = 0; x < MAZE_WIDTH; x++) {
-            if(x%2 == 1 && y%2 == 1){
-                    grid[x][y] = 0;
-            }
-            if(grid[x][y] == 1){
-                drawSquare(x*BLOCK_SIZE, y*BLOCK_SIZE, 0, 255, 255);
-            }
-        }
-    }
-}
-drawMaze();
-*/
