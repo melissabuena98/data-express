98fcf9956452ec2061cae10ff85159e4bc83eb5e
Unknown <Mbuena@student.neumont.edu>
2017-11-27 22:21:12 -0700

    MongoDB user creation stuff

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var expressSession = require('express-session');
 var route = require('./routes/routes.js');
+var expressSession = require('express-session');
 
 var app = express();
 
@@ -11,8 +12,34 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname + '/public')));
 
+var urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
+
+var checkAuth = function(req, res, next){
+    if(req.session.user && req.session.user.isAuthenticated){
+        next();
+    }
+    else{
+        res.redirect('/login');
+    }
+}
+
+app.use(expressSession ({
+    secret: 'ThisIsTheSecret',
+    saveUninitialized: true,
+    resave: true
+}));
+
 app.get('/', route.index);
+app.get('/home', route.home);
 app.get('/login', route.login);
 app.get('/register', route.register);
+app.get('/edit', checkAuth, route.edit);
+
+app.post('/register', urlencodedParser, route.registerUser);
+app.post('/login', urlencodedParser, route.loginUser);
+app.post('/edit/:id', urlencodedParser, route.editUser);
+
 
 app.listen(3000);
--- routes/routes.js
+++ routes/routes.js
@@ -2,20 +2,99 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/data');
 
+var bcrypt = require('bcrypt-nodejs');
+
 var mdb = mongoose.connection;
 mdb.on('error', console.error.bind(console, 'connection error:'));
 mdb.once('open', function (callback) {
 
 });
 
+var userSchema = mongoose.Schema({
+   username: String,
+   password: String,
+   email: String,
+   age: String,
+   answers: Array,
+   type: String
+});
+
+var User = mongoose.model('User_Collection', userSchema);
+
 exports.index = function (req, res) {
       res.render('index');
+};
+
+exports.home = function (req, res){
+    res.render('home');
 };
 
 exports.login = function (req, res) {
     res.render('login');
 };
 
+exports.loginUser = function(req, res){
+    User.findOne({'username': req.body.username}, function(err, user){
+        if(err){
+            console.log(err);
+        }
+        else{
+            var pw = req.body.password;
+            if(bcrypt.compareSync(pw, user.password)){
+                req.session.user={
+                    userID: user._id,
+                    isAuthenticated: true,
+                    userType: user.type,
+                    username: user.username
+                };
+                res.redirect('/home');
+            }
+        }
+    });
+};
+
 exports.register = function (req, res) {
     res.render('register');
+};
+
+exports.registerUser = function (req, res){
+    var hashedPW = bcrypt.hashSync(req.body.password);
+    var user = new User({
+        username: req.body.username,
+        password:hashedPW,
+        email: req.body.email,
+        age: req.body.age,
+        answers: [req.body.degree, req.body.region, req.body.gradYear],
+        type: req.body.accType
+    });
+
+    user.save(function (err, user){
+        if(err) return console.error(err);
+        console.log(req.body.username + ' was added!');
+    });
+    res.redirect('/login');
+};
+
+exports.edit = function (req, res){
+    User.findById(req.session.user.userID, function (err, user) {
+        if (err) return console.error(err);
+        res.render('edit',{
+            user: user
+        });
+    });
+};
+
+exports.editUser = function (req, res){
+    User.findById(req.session.user.userID, function (err, user){
+        if(err) return console.error(err);
+        user.username = req.body.username;
+        user.email = req.body.email;
+        user.age = req.body.age;
+        user.answers = [req.body.degree, req.body.region, req.body.gradYear]
+        user.save(function (err, user){
+            if(err) return console.error(err);
+            console.log(req.body.username + " info updated!");
+        });
+    });
+    res.redirect('/home');
 };
